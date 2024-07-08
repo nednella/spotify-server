e38ebaec468cdaf68f22ee3be91b9cb44db254f5
Ben Allenden <benallenden@protonmail.com>
2024-07-08 19:47:44 +0100

    Refactor: item cap moved to global variable

--- src/common/constants/variables.js
+++ src/common/constants/variables.js
@@ -1,1 +1,2 @@
 export const SPOTIFY_API_PAGINATION_LIMIT = 50 // Range: 0 - 50. Default Spotify API value is 20.
+export const SPOTIFY_API_PAGINATION_ITEM_CAP = 250 // Range: 0 - Infinity. Max. no of items fetched from a paginated endpoint.
